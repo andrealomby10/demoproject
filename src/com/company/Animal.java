b4e570444dd1f04793975e1c8cccda1ddf0bb49f
andrealomby10 <lombardoandrea22@gmail.com>
2020-05-19 16:10:28 +0200

    firts add

---
+++ src/com/company/Animal.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.io.Serializable;
+
+public class Animal implements Serializable {
+public String Name;
+
+    public Animal(String name) {
+        Name = name;
+    }
+}
