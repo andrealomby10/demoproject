035c2fca2ae536af6a25fe28812bde8489396754
andrealomby10 <lombardoandrea22@gmail.com>
2020-05-23 15:12:58 +0200

    firts add

---
+++ src/com/company/Services.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+public interface Services extends Remote {
+
+    public String getDate() throws RemoteException;
+    public String toUP(String s) throws RemoteException;
+    public ArrayList<Person> getList() throws RemoteException;
+    public void addPerson(Person p) throws RemoteException;
+    public void doIntensiveTask() throws RemoteException;
+}
